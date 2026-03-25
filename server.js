c5467754944d83aca3e57385780e98841158618f
Jostin <anonim@gmail.com>
2026-03-25 14:11:51 -0500

    Ultima parte

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ app.get("/", (req, res) => {
 
 res.send("API funcionando 🚀"); }); // Puerto de conexión 
 const PORT = process.env.PORT || 3000; app.listen(PORT, () => { console.log(`Servidor corriendo en el puerto ${PORT}`); });
+await conectionDatabase();
